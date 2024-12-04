6f6824e4957afa0827684a19a5fedb4d52bf0185
Glory Menga <glory.menga@student.ehb.be>
2024-12-04 14:00:15 +0100

    Setup react project folder done

---
+++ Portfolio/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../components/Nav.jsx";
+
+function Home(){
+    return(
+        <div className="container">
+            <Nav />
+        <h1>Glory Menga</h1>
+        <h2>Glory Menga</h2>
+        <p>Glory Menga</p>
+        </div>
+    );
+}
+
+export default Home;
